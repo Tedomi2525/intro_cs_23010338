2fedcf533e78286e15755db4014774cf635f7fb5
Pham Hong Duc <23010338@st.phenikaa-uni.edu.vn>
2023-11-21 08:34:52 +0700

    Them file guessgame

---
+++ guessgame.py
@@ -0,0 +1,8 @@
+from random import randint
+def guess1();
+    secret_number=7
+    guess=int(input("What number am I think of? "))
+    if secret_numer==guess:
+        print("Yay! You got it.")
+    else:
+        print("No, that's not it)
